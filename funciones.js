2c7aa71e596a7c8da72ff2857f0fa3279a345528
nadim500 <duivan5@gmail.com>
2016-01-04 19:30:10 +0000

    funciones en Jvascript

---
+++ funciones.js
@@ -0,0 +1,25 @@
+/*
+
+//Declarar una funcion y la llamas despues
+function saludos(){
+	console.log("hola a todos");
+}
+
+saludos();
+*/
+
+//Declarar una funcion y la llamas ahora
+/*
+(function saludos(){
+	console.log("hola a todos");
+})();
+*/
+
+
+var saludos = function (){
+	console.log("hola a todos");
+}
+
+saludos();
+
+
