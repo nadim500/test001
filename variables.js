e6b51799de86e7871aeed5460f1c00f002c44751
nadim500 <duivan5@gmail.com>
2016-01-04 21:33:57 +0000

    tipos de datos y operadores

---
+++ variables.js
@@ -0,0 +1,20 @@
+//Operadores unarios
+/*console.log(typeof 4.5);
+console.log(typeof "duivan");
+console.log(typeof 'a');
+console.log(10+2);
+//Boolean values
+console.log(3>5);
+console.log(4>2),
+console.log("Aardvark" < "Zoroaster");
+console.log("Dota"=="Dota");
+console.log(NaN==NaN);
+*/
+console.log(1 + 1 == 2 && 10 * 10 > 50);
+console.log(true?1:2);
+console.log(false?1:2);
+console.log(null == 0);
+console.log(false == 0);
+console.log(null || "user");
+console.log("karl" || "user");
+console.log("user"||null);
