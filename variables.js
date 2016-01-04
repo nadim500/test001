af5a4e286273e899a4d18dcee6ecd697e46e55be
nadim500 <duivan5@gmail.com>
2016-01-04 21:38:36 +0000

    Tipos de datos y variables

--- variables.js
+++ variables.js
@@ -1,5 +1,5 @@
 //Operadores unarios
-/*console.log(typeof 4.5);
+console.log(typeof 4.5);
 console.log(typeof "duivan");
 console.log(typeof 'a');
 console.log(10+2);
@@ -9,7 +9,6 @@ console.log(4>2),
 console.log("Aardvark" < "Zoroaster");
 console.log("Dota"=="Dota");
 console.log(NaN==NaN);
-*/
 console.log(1 + 1 == 2 && 10 * 10 > 50);
 console.log(true?1:2);
 console.log(false?1:2);
@@ -17,4 +16,4 @@ console.log(null == 0);
 console.log(false == 0);
 console.log(null || "user");
 console.log("karl" || "user");
-console.log("user"||null);
+console.log("user"|| null);
