4b7513358d7505146ccf5ba9706b913ee6b9a430
nadim500 <duivan5@gmail.com>
2016-01-05 00:06:00 +0000

    Creacion de funciones

--- funciones.js
+++ funciones.js
@@ -21,4 +21,22 @@ var saludos = function (){
 }
 saludos();
 */
-
+//ejemplo:
+var square = function(x){
+	return x*x;
+};
+console.log(square(25));
+//ejemplo:
+var makeNoise = function(){
+	console.log("Pling!");
+};
+makeNoise();
+//ejemplo:
+var power=function(base,exponent){
+	var result=1;
+	for(var count=0;count<exponent;count++){
+		result *= base;
+	}
+	return result;
+};
+console.log(power(2,10));
